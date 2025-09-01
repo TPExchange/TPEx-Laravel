0c71b76fb62f76272ded3d14e191a734222411bd
Cyclic3 <cyclic3.git@gmail.com>
2025-09-01 16:39:28 +0100

    Add in missing use

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
