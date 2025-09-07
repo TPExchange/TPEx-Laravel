57e47bd10ff5aa13a194978b6d1c9fafe9e378b7
Cyclic3 <cyclic3.git@gmail.com>
2025-09-07 14:05:04 +0100

    Added market history graph

--- resources/views/items/info.blade.php
+++ resources/views/items/info.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
     <h3 class="text-center text-4xl mb-10">{{ucwords(str_replace("_", " ", $asset))}} Information</h3>
+    <x-item-history :$history :$asset />
     <x-item-info :$buy :$sell :$asset />
 </x-layout>
