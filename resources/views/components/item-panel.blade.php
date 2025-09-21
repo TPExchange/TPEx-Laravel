7ecde4ca689788b8f4af6604364aa5acf0c37626
Cyclic3 <cyclic3.git@gmail.com>
2025-09-21 23:50:19 +0100

    Add more diamond shepherding

--- resources/views/components/item-panel.blade.php
+++ resources/views/components/item-panel.blade.php
@@ -1,6 +1,7 @@
 @props(["item_name"=>"Item Name", "count"=>"0", "name"=>"minecraft_cobblestone", "sys_id"=>"cobblestone", "price"=>null, "restricted"])
 @php
         $img_id = "minecraft_" . $name;
+        $info_url = ($name == "diamond") ? "/exchange-coins" : "items/info?item=$name";
 @endphp
 
 @if (in_array($name, $restricted))
@@ -9,7 +10,7 @@
 <div class="w-100 bg-neutral-300 p-2 rounded-md">
 @endif
         <div class="flex gap-5">
-                <x-item-img item="minecraft_{{ $name }}" linkto="/items/info?item={{ $name }}"/>
+                <x-item-img item="minecraft_{{ $name }}" linkto="{{ $info_url }}"/>
                 <div class="flex flex-col flex-1 items-center sm:items-start">
                         <h3 class="font-bold text-xl mb-2"><a href="/items/info?item={{ $name }}">{{ $item_name }}</a></h3>
 
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     @vite(["resources/js/app.js", "resources/css/app.css"])
-    
+
 </head>
 
 <body class="font-hanken-grotesk">
@@ -23,6 +23,7 @@
             <div class="md:absolute md:top-0 md:left-0">
                 <a href="/">
                     <img src="/images/TPEx.png" class="w-25" alt="" />
+                    dev
                 </a>
             </div>
 
