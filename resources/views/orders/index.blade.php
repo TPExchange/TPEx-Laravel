11d878336d1065eea0c9ae6b0d518c5171d5cb5a
Cyclic3 <cyclic3.git@gmail.com>
2025-09-30 18:33:13 +0100

    Prioritise buy order

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -11,29 +11,6 @@
             </div>
         @endif
 
-
-    <div class="bg-neutral-100 rounded-lg my-10">
-        <x-section-header>
-            @if (request("q"))
-                Your Sell Orders Matching: "{{ htmlspecialchars(request("q")) }}"
-            @else
-                Your Sell Orders
-            @endif
-        </x-section-header>
-
-        <div class="flex flex-wrap gap-3 mb-2">
-        @foreach ($sell_orders as $id=>$order)
-            @php
-                $name = $order["asset"];
-                $nicename = ucwords(str_replace("_", " ", $name));
-                $count = $order["amount_remaining"];
-                $price = $order["coins"];
-            @endphp
-            <x-order-panel item_name="{{ $nicename }}" name="{{ $name }}" count="{{ $count }}" price="{{ $price }}" id="{{ $id }}" :$restricted/>
-        @endforeach
-        </div>
-    </div>
-
     <div class="bg-neutral-100 rounded-lg my-10">
         <x-section-header>
             @if (request("q"))
@@ -57,5 +34,25 @@
     </div>
 
 
+    <div class="bg-neutral-100 rounded-lg my-10">
+        <x-section-header>
+            @if (request("q"))
+                Your Sell Orders Matching: "{{ htmlspecialchars(request("q")) }}"
+            @else
+                Your Sell Orders
+            @endif
+        </x-section-header>
 
+        <div class="flex flex-wrap gap-3 mb-2">
+        @foreach ($sell_orders as $id=>$order)
+            @php
+                $name = $order["asset"];
+                $nicename = ucwords(str_replace("_", " ", $name));
+                $count = $order["amount_remaining"];
+                $price = $order["coins"];
+            @endphp
+            <x-order-panel item_name="{{ $nicename }}" name="{{ $name }}" count="{{ $count }}" price="{{ $price }}" id="{{ $id }}" :$restricted/>
+        @endforeach
+        </div>
+    </div>
 </x-layout>
