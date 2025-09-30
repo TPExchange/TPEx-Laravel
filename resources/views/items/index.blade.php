f17b7505453b000998a72d847f427ab8e50ee564
Cyclic3 <cyclic3.git@gmail.com>
2025-09-30 18:34:24 +0100

    Flip over buy orders on main page

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -8,34 +8,6 @@
     <div class="bg-neutral-100 rounded-lg my-10">
         <x-section-header>
             @if (request("q"))
-                Items For Sale Matching: "{{ htmlspecialchars(request("q")) }}"
-            @else
-                Items For Sale
-            @endif
-        </x-section-header>
-
-        <div class="flex gap-3 mb-2 p-5 flex-wrap">
-        @foreach ($sell_orders as $name => $prices)
-            @php
-                $nicename = ucwords(str_replace("_", " ", $name));
-                $count = 0;
-                foreach ($prices as $price=>$orders) {
-                    foreach ($orders as $order) {
-                        $count += $order["amount_remaining"];
-                    }
-                }
-                $price = array_keys($prices)[0];
-            @endphp
-            @if ($count > 0)
-                <x-item-panel item_name="{{ $nicename }}" name="{{ $name }}" count="{{ $count }}" price="{{ $price }}" :$restricted/>
-            @endif
-        @endforeach
-        </div>
-    </div>
-
-    <div class="bg-neutral-100 rounded-lg my-10">
-        <x-section-header>
-            @if (request("q"))
                 Items In Demand Matching: "{{ htmlspecialchars(request("q")) }}"
             @else
                 Items In Demand
@@ -61,5 +33,32 @@
     </div>
 
 
+    <div class="bg-neutral-100 rounded-lg my-10">
+        <x-section-header>
+            @if (request("q"))
+                Items For Sale Matching: "{{ htmlspecialchars(request("q")) }}"
+            @else
+                Items For Sale
+            @endif
+        </x-section-header>
+
+        <div class="flex gap-3 mb-2 p-5 flex-wrap">
+        @foreach ($sell_orders as $name => $prices)
+            @php
+                $nicename = ucwords(str_replace("_", " ", $name));
+                $count = 0;
+                foreach ($prices as $price=>$orders) {
+                    foreach ($orders as $order) {
+                        $count += $order["amount_remaining"];
+                    }
+                }
+                $price = array_keys($prices)[0];
+            @endphp
+            @if ($count > 0)
+                <x-item-panel item_name="{{ $nicename }}" name="{{ $name }}" count="{{ $count }}" price="{{ $price }}" :$restricted/>
+            @endif
+        @endforeach
+        </div>
+    </div>
 
 </x-layout>
