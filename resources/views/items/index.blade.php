c9ed5291e75101609464a06406e29b951dbe193c
Cyclic3 <cyclic3.git@gmail.com>
2025-10-01 11:33:52 +0100

    Sort orders by recency of best price

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -15,18 +15,28 @@
         </x-section-header>
 
         <div class="flex gap-3 mb-2 p-5 flex-wrap">
-        @foreach ($buy_orders as $name => $prices)
-            @php
+        @php
+            $buy_disp = array();
+            foreach($buy_orders as $name => $prices) {
                 $nicename = ucwords(str_replace("_", " ", $name));
                 $count = 0;
+                $best_order = 0;
+                $best_price = array_key_last($prices);
                 foreach ($prices as $price=>$orders) {
                     foreach ($orders as $order) {
+                        if ($best_price == $price) {
+                            $best_order = max($best_order, $order["id"]);
+                        }
                         $count += $order["amount_remaining"];
                     }
                 }
-            @endphp
+                array_push($buy_disp, ["count"=>$count, "price"=>$best_price, "name"=>$name, "nicename"=>$nicename, "best_order" => $best_order]);
+            }
+            usort($buy_disp, function($a, $b) { return $b["best_order"] - $a["best_order"]; })
+        @endphp
+        @foreach ($buy_disp as $elem)
             @if ($count > 0)
-                <x-item-panel item_name="{{ $nicename }}" name="{{ $name }}" count="{{ $count }}" price="{{ $price }}" :$restricted/>
+                <x-item-panel item_name="{{ $elem['nicename'] }}" name="{{ $elem['name'] }}" count="{{ $elem['count'] }}" price="{{ $elem['price'] }}" :$restricted/>
             @endif
         @endforeach
         </div>
@@ -43,19 +53,28 @@
         </x-section-header>
 
         <div class="flex gap-3 mb-2 p-5 flex-wrap">
-        @foreach ($sell_orders as $name => $prices)
-            @php
+        @php
+            $sell_disp = array();
+            foreach($sell_orders as $name => $prices) {
                 $nicename = ucwords(str_replace("_", " ", $name));
                 $count = 0;
+                $best_order = 0;
+                $best_price = array_key_first($prices);
                 foreach ($prices as $price=>$orders) {
                     foreach ($orders as $order) {
+                        if ($best_price == $price) {
+                            $best_order = max($best_order, $order["id"]);
+                        }
                         $count += $order["amount_remaining"];
                     }
                 }
-                $price = array_keys($prices)[0];
-            @endphp
+                array_push($sell_disp, ["count"=>$count, "price"=>$best_price, "name"=>$name, "nicename"=>$nicename, "best_order" => $best_order]);
+            }
+            usort($sell_disp, function($a, $b) { return $b["best_order"] - $a["best_order"]; })
+        @endphp
+        @foreach ($sell_disp as $elem)
             @if ($count > 0)
-                <x-item-panel item_name="{{ $nicename }}" name="{{ $name }}" count="{{ $count }}" price="{{ $price }}" :$restricted/>
+                <x-item-panel item_name="{{ $elem['nicename'] }}" name="{{ $elem['name'] }}" count="{{ $elem['count'] }}" price="{{ $elem['price'] }}" :$restricted/>
             @endif
         @endforeach
         </div>
