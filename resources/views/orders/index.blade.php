1007744fa9980c8d5b15cbe5dbcfa3a6995ff497
Cyclic3 <cyclic3.git@gmail.com>
2025-10-02 15:38:46 +0100

    Sort orders chronologically

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -21,15 +21,20 @@
         </x-section-header>
 
         <div class="flex gap-3 mb-2 p-5 flex-wrap">
-        @foreach ($buy_orders as $id=>$order)
             @php
-                $name = $order["asset"];
-                $nicename = ucwords(str_replace("_", " ", $name));
-                $count = $order["amount_remaining"];
-                $price = $order["coins"];
+                $buy_disp = array();
+                foreach($buy_orders as $id => $order) {
+                    $name = $order["asset"];
+                    $nicename = ucwords(str_replace("_", " ", $name));
+                    $count = $order["amount_remaining"];
+                    $price = $order["coins"];
+                    array_push($buy_disp, ["count"=>$count, "price"=>$price, "name"=>$name, "nicename"=>$nicename, "id" => $id]);
+                }
+                usort($buy_disp, function($a, $b) { return $b["id"] - $a["id"]; })
             @endphp
-            <x-order-panel item_name="{{ $nicename }}" name="{{ $name }}" count="{{ $count }}" price="{{ $price }}" id="{{ $id }}" :$restricted/>
-        @endforeach
+            @foreach ($buy_disp as $order)
+                <x-order-panel item_name="{{ $order['nicename'] }}" name="{{ $order['name'] }}" count="{{ $order['count'] }}" price="{{ $order['price'] }}" id="{{ $order['id'] }}" :$restricted/>
+            @endforeach
         </div>
     </div>
 
@@ -44,15 +49,20 @@
         </x-section-header>
 
         <div class="flex gap-3 mb-2 p-5 flex-wrap">
-        @foreach ($sell_orders as $id=>$order)
             @php
-                $name = $order["asset"];
-                $nicename = ucwords(str_replace("_", " ", $name));
-                $count = $order["amount_remaining"];
-                $price = $order["coins"];
+                $sell_disp = array();
+                foreach($sell_orders as $id => $order) {
+                    $name = $order["asset"];
+                    $nicename = ucwords(str_replace("_", " ", $name));
+                    $count = $order["amount_remaining"];
+                    $price = $order["coins"];
+                    array_push($sell_disp, ["count"=>$count, "price"=>$price, "name"=>$name, "nicename"=>$nicename, "id" => $id]);
+                }
+                usort($sell_disp, function($a, $b) { return $b["id"] - $a["id"]; })
             @endphp
-            <x-order-panel item_name="{{ $nicename }}" name="{{ $name }}" count="{{ $count }}" price="{{ $price }}" id="{{ $id }}" :$restricted/>
-        @endforeach
+            @foreach ($sell_disp as $order)
+                <x-order-panel item_name="{{ $order['nicename'] }}" name="{{ $order['name'] }}" count="{{ $order['count'] }}" price="{{ $order['price'] }}" id="{{ $order['id'] }}" :$restricted/>
+            @endforeach
         </div>
     </div>
 </x-layout>
