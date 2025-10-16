f032b8a6496c5e352df797a9f63673861d4da15c
AliceeeSixes <6d6perturn@gmail.com>
2025-10-16 10:40:41 +0100

    fix left-over dashboard tile issues

--- resources/views/admin/deposits_withdrawals.blade.php
+++ resources/views/admin/deposits_withdrawals.blade.php
@@ -5,8 +5,8 @@
 
     <section class="mt-10">
         <div class="grid lg:grid-cols-2 gap-5 h-60">
-            <x-dashboard-tile colour="red" href="/admin/deposits" icon="arrow-down">Deposits</x-dashboard-tile>
-            <x-dashboard-tile colour="blue" href="/admin/withdrawals" icon="arrow-up">Withdrawals</x-dashboard-tile>
+            <x-dashboard-tile class="bg-red-300" href="/admin/deposits" icon="arrow-down">Deposits</x-dashboard-tile>
+            <x-dashboard-tile class="bg-blue-300" href="/admin/withdrawals" icon="arrow-up">Withdrawals</x-dashboard-tile>
         </div>
     </section>
 </x-layout>
